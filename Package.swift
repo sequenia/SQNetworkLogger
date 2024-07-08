019f5f436832c0583c9debcb0f9fc9e82e5aed0f
Ivan Mikhailovskii <>
2024-07-08 11:05:46 +0700

    bump: up version moya 15.0.3

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["SQNetworkLogger"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "14.0.0"))
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.3"))
     ],
     targets: [
         .target(
